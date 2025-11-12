326c743a6ed508ce2fed5b02371270d0816b74ad
THUILLIERColin <colin.thuillier@gmail.com>
2025-11-12 14:10:35 +0100

    feat(formations): add formation page with links to videos
    * Created a new `Page` component for the formations section.
    * Added links to three video resources: Fundamentals, Server Components + Prisma, and Server Function + Mutation.
    * Introduced `PageLayout`, `Card`, and related components for consistent UI structure.

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,21 @@
+import PageLayout from '@/components/layout';
 import { ModeToggle } from '@/components/theme-toggle';
+import Link from 'next/link';
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center font-sans">
-      <h1>Init project</h1>
-      <ModeToggle />
-    </div>
+    <PageLayout>
+      <main className="m-auto p-4">
+        <h1 className="text-4xl font-bold mb-4">Welcome to the Home Page</h1>
+        <p className="mb-4">This is the main landing page of the application.</p>
+        <ModeToggle />
+        <Link
+          href="/formations"
+          className="text-indigo-500 underline mt-6 block"
+        >
+          Formations
+        </Link>
+      </main>
+    </PageLayout>
   );
 }
